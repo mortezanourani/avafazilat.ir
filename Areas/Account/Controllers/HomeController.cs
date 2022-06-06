e2c1c608bdd379d597ebf4fdfb5488a244ad9ad3
Morteza Nourani <mortizanourani@gmail.com>
2022-06-06 23:45:25 +0430

    Add register page
    - Register page added. It includes registration proccess and error
      returning.

--- Areas/Account/Controllers/HomeController.cs
+++ Areas/Account/Controllers/HomeController.cs
@@ -1,10 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Fazilat.Areas.Account.Models;
+using Fazilat.Models;
 
 namespace Fazilat.Areas.Account.Controllers
 {
     [Area("Account")]
     public class HomeController : Controller
     {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IUserStore<ApplicationUser> _userStore;
+        private readonly IUserEmailStore<ApplicationUser> _emailStore;
+
+        public HomeController(
+            SignInManager<ApplicationUser> signInManager,
+            UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            IUserStore<ApplicationUser> userStore)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _userStore = userStore;
+            _emailStore = (IUserEmailStore<ApplicationUser>)_userStore; 
+        }
+
         public IActionResult Index()
         {
             if (!User.Identity.IsAuthenticated)
@@ -13,6 +37,44 @@ namespace Fazilat.Areas.Account.Controllers
             }
 
             return View();
+        }
+
+        public IActionResult Register()
+        {
+            RegisterModel model = new RegisterModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = new ApplicationUser();
+            await _userStore.SetUserNameAsync(user, model.Email, CancellationToken.None);
+            await _emailStore.SetEmailAsync(user, model.Email, CancellationToken.None);
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                var role = await _roleManager.FindByNameAsync("User");
+                if (role != null)
+                {
+                    await _userManager.AddToRoleAsync(user, role.Name);
+                }
+
+                await _signInManager.SignInAsync(user, isPersistent: false);
+                return RedirectToAction("Index");
+            }
+
+            foreach(var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+            return View(model);
         }
     }
 }
