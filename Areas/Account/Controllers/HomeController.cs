596795e69c3e311b86f90778dc0e6a175a60841a
Morteza Nourani <mortizanourani@gmail.com>
2022-06-17 16:56:56 +0430

    Add personal information page
    - Personal information page added. It includes first name, last name,
      national code and persian birth date.
    - InformationModel added to set validations for personal information
      page inputs.

--- Areas/Account/Controllers/HomeController.cs
+++ Areas/Account/Controllers/HomeController.cs
@@ -1,9 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Globalization;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Fazilat.Areas.Account.Models;
 using Fazilat.Models;
+using Fazilat.Data;
 
 namespace Fazilat.Areas.Account.Controllers
 {
@@ -15,18 +20,21 @@ namespace Fazilat.Areas.Account.Controllers
         private readonly RoleManager<IdentityRole> _roleManager;
         private readonly IUserStore<ApplicationUser> _userStore;
         private readonly IUserEmailStore<ApplicationUser> _emailStore;
+        private readonly ApplicationDbContext _context;
 
         public HomeController(
             SignInManager<ApplicationUser> signInManager,
             UserManager<ApplicationUser> userManager,
             RoleManager<IdentityRole> roleManager,
-            IUserStore<ApplicationUser> userStore)
+            IUserStore<ApplicationUser> userStore,
+            ApplicationDbContext context)
         {
             _signInManager = signInManager;
             _userManager = userManager;
             _roleManager = roleManager;
             _userStore = userStore;
-            _emailStore = (IUserEmailStore<ApplicationUser>)_userStore; 
+            _emailStore = (IUserEmailStore<ApplicationUser>)_userStore;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -143,6 +151,85 @@ namespace Fazilat.Areas.Account.Controllers
                 ModelState.AddModelError(string.Empty, error.Description);
             }
             return View(passwordModel);
+        }
+
+        public async Task<IActionResult> PersonalInfo()
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Login");
+            }
+
+            var user = await _userManager.GetUserAsync(User);
+
+            var userInfo = await _context.Information
+                .FirstOrDefaultAsync(i => i.UserId == user.Id);
+            if (userInfo == null)
+            {
+                userInfo = new UserInformation();
+                userInfo.UserId = user.Id;
+                await _context.AddAsync(userInfo);
+                await _context.SaveChangesAsync();
+            }
+
+            PersianCalendar persianCalendar = new PersianCalendar();
+            DateTime birthDate = (DateTime) userInfo.BirthDate;
+            var day = persianCalendar.GetDayOfMonth(birthDate);
+            var month = persianCalendar.GetMonth(birthDate);
+            var year = persianCalendar.GetYear(birthDate);
+
+            InformationModel personalInfo = new InformationModel()
+            {
+                NationalCode = userInfo.NationalCode,
+                FirstName = userInfo.FirstName,
+                LastName = userInfo.LastName,
+                Day = day,
+                Month = month,
+                Year = year
+            };
+            return View(personalInfo);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PersonalInfo(InformationModel personalInfo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(personalInfo);
+            }
+
+            var user = await _userManager.GetUserAsync(User);
+
+            PersianCalendar persianCalendar = new PersianCalendar();
+            var birthDate = persianCalendar.ToDateTime(
+                personalInfo.Year,
+                personalInfo.Month,
+                personalInfo.Day,
+                0, 0, 0, 0);
+
+            var userInfo = new UserInformation()
+            {
+                UserId = user.Id,
+                NationalCode = personalInfo.NationalCode,
+                FirstName = personalInfo.FirstName,
+                LastName = personalInfo.LastName,
+                BirthDate = birthDate,
+            };
+
+            try
+            {
+                _context.Attach(userInfo).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+                TempData.Add("StatusMessage", "Information updated successfully.");
+                return RedirectToAction();
+            }
+            catch (Exception exception)
+            {
+                TempData.Add("StatusMessage", exception.Message);
+                return RedirectToAction();
+            }
+
+            return View();
         }
     }
 }
---
+++ Areas/Account/Models/InformationModel.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Http;
+using Fazilat.Models;
+using System;
+
+#nullable disable
+namespace Fazilat.Areas.Account.Models
+{
+    public class InformationModel : UserInformation
+    {
+        [RegularExpression("^[0-9]*$", ErrorMessage = "National code is invalid.")]
+        [StringLength(10, ErrorMessage = "The {0} must has {1} digits.", MinimumLength = 10)]
+        public string NationalCode { get; set; }
+
+        [RegularExpression("^[آ-یای]+$", ErrorMessage = "Invalid first name.")]
+        [StringLength(100, ErrorMessage = "The {0} must has {1} max character length.")]
+        public string FirstName { get; set; }
+
+        [RegularExpression("^[آ-یای]+$", ErrorMessage = "Invalid last name.")]
+        [StringLength(100, ErrorMessage = "The {0} must has {1} max character length.")]
+        public string LastName { get; set; }
+
+        //public new DateTime? BirthDate { get; set; }
+
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Enter valid number.")]
+        [Range(1, 31, ErrorMessage = "Enter valid number.")]
+        public int Day { get; set; }
+
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Enter valid number.")]
+        [Range(1, 12, ErrorMessage = "Enter valid number.")]
+        public int Month { get; set; }
+
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Enter valid number.")]
+        [Range(1300, 1500, ErrorMessage = "Enter valid number.")]
+        public int Year { get; set; }
+    }
+}
