4d7f8babcc8fd12afc967aea589b797350b6f510
Morteza Nourani <mortezanourani@gmail.com>
2024-03-10 13:03:28 +0330

    Upgrade Project
    - Project upgraded from .Net 3.1 to .Net 8.
    - The connectionString value updated to use database on new
      environment

---
+++ Areas/Account/Models/Login.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Fazilat.Areas.Account.Models
+{
+    public partial class Login
+    {
+        [Required]
+        [Display(Name = "نام کاربری")]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "رمز عبور")]
+        public string Password { get; set; }
+    }
+}
