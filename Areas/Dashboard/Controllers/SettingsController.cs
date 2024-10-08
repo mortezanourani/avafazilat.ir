a182f080b9dbd7ef100118d84a59596ee42af11f
Morteza Nourani <mortezanourani@gmail.com>
2024-10-09 00:42:24 +0330

    Add Profile Settings Page

---
+++ Areas/Dashboard/Controllers/SettingsController.cs
@@ -0,0 +1,39 @@
+﻿using Fazilat.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Fazilat.Areas.Dashboard.Controllers;
+
+[Area("Dashboard")]
+[Authorize]
+public class SettingsController : Controller
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public SettingsController(
+        UserManager<ApplicationUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    [Route("Dashboard/Settings/")]
+    public async Task<IActionResult> Index()
+    {
+        ApplicationUser user = await _userManager.FindByNameAsync(User.Identity.Name);
+        return View(user);
+    }
+
+    [Authorize("Administrator")]
+    [Route("Dashboard/Settings/{id?}")]
+    public async Task<IActionResult> Index(string id)
+    {
+        ApplicationUser user = await _userManager.FindByIdAsync(id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+        return View(user);
+    }
+}
--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Fazilat.Models;
@@ -7,20 +8,25 @@ namespace Fazilat.Models;
 public class ApplicationUser : IdentityUser
 {
     [PersonalData]
+    [Display(Name = "نام")]
     public string FirstName { get; set; }
 
     [PersonalData]
+    [Display(Name = "نام خانوادگی")]
     public string LastName { get; set; }
 
     [PersonalData]
+    [Display(Name = "تاریخ تولد")]
     public DateTime? BirthDate { get; set; }
 
     [NotMapped]
     public string Expired { get; set; }
 
     [NotMapped]
+    [Display(Name = "تاریخ اتمام عضویت")]
     public string Expiration { get; set; }
 
     [PersonalData]
+    [Display(Name = "تاریخ ثبت نام")]
     public DateTime Registered { get; set; }
 }
