2ae5180f56b90c0fa687f3e927ac449a071c9a96
Morteza Nourani <mortezanourani@gmail.com>
2024-10-12 13:14:38 +0330

    Fix Settings Page
    - Settings model errors display updated.

--- Areas/Dashboard/Controllers/SettingsController.cs
+++ Areas/Dashboard/Controllers/SettingsController.cs
@@ -44,80 +44,35 @@ public class SettingsController : Controller
         settings.Profile = profile;
         settings.Communication = communication;
 
-
-        if (TempData.Count > 0)
-        {
-            foreach (var error in TempData)
-            {
-                ModelState.AddModelError(string.Empty, error.Value.ToString());
-            }
-        }
         return View(settings);
     }
 
     [HttpPost]
-    [Route("Dashboard/Settings/UpdateProfile/")]
-    public async Task<IActionResult> UpdateProfile([Bind(Prefix = "Profile")] SettingsProfile profile)
+    [Route("Dashboard/Settings/")]
+    public async Task<IActionResult> Index(SettingsViewModel settings)
     {
+        ApplicationUser user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+        SettingsProfile profile = new SettingsProfile();
+        profile.Id = user.Id;
+        profile.UserName = user.UserName;
+        profile.FirstName = user.FirstName;
+        profile.LastName = user.LastName;
+        profile.BirthDate = user.BirthDate.ToString();
+
+        SettingsCommunication communication = new SettingsCommunication();
+        communication.PhoneNumber = user.PhoneNumber;
+        communication.Email = user.Email;
+
+        SettingsViewModel model = new SettingsViewModel();
+        model.Profile = profile;
+        model.Communication = communication;
+
         if (!ModelState.IsValid)
         {
-            foreach (var modelState in ModelState.Values)
-            {
-                foreach (ModelError error in modelState.Errors)
-                {
-                    TempData.Add(error.ErrorMessage, error.ErrorMessage);
-                }
-            }
+            return View(model);
         }
 
         return RedirectToAction("Index");
     }
-
-    [HttpPost]
-    [Route("Dashboard/Settings/UpdateCommunication/")]
-    public async Task<IActionResult> UpdateCommunication([Bind(Prefix = "Comunication")] SettingsCommunication communication)
-    {
-        if (!ModelState.IsValid)
-        {
-            return View("ModelError");
-        }
-
-        ModelState.AddModelError(string.Empty, "This is test error.");
-        ModelState.AddModelError(string.Empty, "This is second test error.");
-        foreach (ModelError error in ModelState.Values.FirstOrDefault().Errors)
-        {
-            TempData.Add(error.ErrorMessage, error.ErrorMessage);
-        }
-        return RedirectToAction("Index");
-    }
-
-    [HttpPost]
-    [Route("Dashboard/Settings/ChangePassword/")]
-    public async Task<IActionResult> ChangePassword([Bind(Prefix = "Password")] SettingsPassword password)
-    {
-        if (!ModelState.IsValid)
-        {
-            return View("ModelError");
-        }
-
-        ModelState.AddModelError(string.Empty, "This is test error.");
-        ModelState.AddModelError(string.Empty, "This is second test error.");
-        foreach (ModelError error in ModelState.Values.FirstOrDefault().Errors)
-        {
-            TempData.Add(error.ErrorMessage, error.ErrorMessage);
-        }
-        return RedirectToAction("Index");
-    }
-
-    //[Authorize(Roles = "Administrator")]
-    //[Route("Dashboard/Settings/{id?}")]
-    //public async Task<IActionResult> Index(string id)
-    //{
-    //    ApplicationUser user = await _userManager.FindByIdAsync(id);
-    //    if (user == null)
-    //    {
-    //        return NotFound();
-    //    }
-    //    return View(user);
-    //}
 }
